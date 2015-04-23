7579275867d83283ae91b92f22b3b20f443404eb
Kevin Wyatt <kwyatt187@gmail.com>
2015-04-22 22:46:16 -0500

    Updated to use MongoDB

--- offTheGrid.py
+++ offTheGrid.py
@@ -1,8 +1,9 @@
 import smtplib
 from email.mime.text import MIMEText
-import sqlite3
 import os
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask.ext.pymongo import PyMongo
+from bson.objectid import ObjectId
 from contextlib import closing
 from flask_googlemaps import GoogleMaps
 import config
@@ -10,25 +11,8 @@ import config
 app = Flask(__name__)
 app.config.from_object('config')
 GoogleMaps(app)
+mongo = PyMongo(app)
 
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
 
 @app.route('/')
 def home():
@@ -60,8 +44,7 @@ def buy_ad():
         flash('Ad request sent')
         return redirect(url_for('buy_ad'))
     else:
-        cur = g.db.execute("select * from locations")
-        locations = [str(row[0]) for row in cur.fetchall()]
+        locations = mongo.db.locations.find()
         return render_template('buyad.html', locations=locations)
 
 @app.route('/bookevent', methods=['GET', 'POST'])
@@ -84,8 +67,7 @@ def book_event():
         flash('Booking request sent')
         return render_template('bookevent.html')
     else:
-        cur = g.db.execute("select * from locations")
-        locations = [str(row[0]) for row in cur.fetchall()]
+        locations = mongo.db.locations.find()
         return render_template('bookevent.html', locations=locations)
 
 @app.route('/afterparty')
@@ -93,8 +75,7 @@ def after_party():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        cur = g.db.execute('select * from afterparty order by id desc')
-        afterparties = [dict(location=row[1],date=row[2],description=row[3],submittedby=row[4]) for row in cur.fetchall()]
+        afterparties = mongo.db.afterparties.find().sort([( '_id', -1)])
         return render_template('afterparty.html', afterparties=afterparties)
 
 @app.route('/addafterparty', methods=['GET', 'POST'])
@@ -103,9 +84,10 @@ def add_after_party():
         return render_template('login.html')
     else:
         if request.method == 'POST':
-            g.db.execute('insert into afterparty (location,date,description,username) values (?, ?, ?, ?)',
-                         [request.form['location'], request.form['date'], request.form['description'], request.form['username']])
-            g.db.commit()
+            mongo.db.afterparties.insert({'location': request.form['location'],
+                                          'date': request.form['date'],
+                                          'description': request.form['description'],
+                                          'submittedby': session['username']})
             return redirect(url_for('after_party'))
         else:
             return render_template('addafterparty.html')
@@ -117,26 +99,33 @@ def edit_after_parties():
     else:
         if request.method == 'POST':
             if 'update' in request.form:
-                g.db.execute('update afterparty set location=?,date=?,description=? where id == ?',
-                             [request.form['location'], request.form['date'], request.form['description'], request.form['id']])
+                mongo.db.afterparties.update({ '_id' : ObjectId(request.form['_id']),
+                                               'submittedby' : session['username'] },
+                                             { 'location' : request.form['location'],
+                                               'date' : request.form['date'],
+                                               'description' : request.form['description'],
+                                               'submittedby' : session['username']},
+                                             upsert=True  )
+                                             
             elif 'delete' in request.form:
-                g.db.execute('delete from afterparty where id == ?', request.form['id'])
+                mongo.db.afterparties.remove({ '_id' : ObjectId(request.form['_id']) ,
+                                               'submittedby' : session['username'] })
 
-            g.db.commit()
             return redirect(url_for('edit_after_parties'))
         else:
-            cur = g.db.execute('select * from afterparty where username == ? order by id desc', [session['username']])
-            afterparties = [dict(id=row[0],location=row[1],date=row[2],description=row[3],submittedby=row[4]) for row in cur.fetchall()]
+            afterparties = mongo.db.afterparties.find({'submittedby' : session['username']}).sort([( '_id', -1)])
             return render_template('myafterparties.html', afterparties=afterparties)
             
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        cur = g.db.execute("select username,password from users where username == ? and password == ?",
-                           [request.form['username'],request.form['password']])
-        if len(cur.fetchall()) == 0:
-            error = "Invalid username or password"
+        user = mongo.db.users.find({'username' : request.form['username']})
+        if user.count() == 0:
+            error = "Invalid username"
+            return render_template('login.html', error=error)
+        elif user[0]['password'] != request.form['password']:
+            error = "Invalid  password"
             return render_template('login.html', error=error)
         else:
             session['logged_in'] = True
@@ -156,23 +145,56 @@ def logout():
 def signup():
     error = None
     if request.method == 'POST':
-        cur = g.db.execute('select username from users where username == ?',
-                           [request.form['username']])
-        if len(cur.fetchall()) > 0:
+        user = mongo.db.users.find({'username' : request.form['username']})
+        email = mongo.db.users.find({'email' : request.form['email']})
+        if user.count() > 0:
             error = "Username already exists"
             return render_template('signup.html', error=error)
+        elif request.form['password1'] != request.form['password2']:
+            error = "Passwords do not match"
+            return render_template('signup.html', error=error)
+        elif email.count() > 0:
+            error = "A username with that email already exists"
+            return render_template('signup.html', error=error)
         else:
-            g.db.execute('insert into users (username,password,email) values (?, ?, ?)',
-                         [request.form['username'], request.form['password'], request.form['email']])
-            g.db.commit()
+            mongo.db.users.insert({'username' : request.form['username'],
+                                   'password' : request.form['password1'],
+                                   'email' : request.form['email']})
+
             session['logged_in'] = True
             session['username'] = request.form['username']
             return redirect(url_for('after_party'))
     else:
         return render_template('signup.html', error=error)
+
+@app.route('/forgotpassword', methods=['GET','POST'])
+def forgot_password():
+    error = None
+    if request.method == 'POST':
+        user = mongo.db.users.find({'email' : request.form['email']})
+        if user.count() == 0:
+            error = "Could not find account for email: '"+request.form['email']+"'"
+            return render_template("forgotpassword.html", error=error)
+        else:
+            credentials = "Account credentials for Off The Grid Advertising:\n"
+            credentials +="Username: "+ user[0]['username']+"\n"
+            credentials +="Password: "+ user[0]['password']+"\n"
+
+            msg = MIMEText(credentials)
+            msg['Subject'] = "Forgot Password"
+            msg['From'] = "Off The Grid Advertising"
+            msg['To'] = request.form['email']
+            
+            s = smtplib.SMTP('localhost')
+            s.sendmail("Off_The_Grid_Account@offthegrid.com", [request.form['email']], msg.as_string())
+            s.quit()
+            flash("Email with password sent to "+request.form['email']+". Don't forget to check your spam.")
+            return redirect(url_for('forgot_password'))
+    else:
+        return render_template('forgotpassword.html', error=error)
 # @app.route('/calendar')
 # def calendar():
 #     return render_template('calendar.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
